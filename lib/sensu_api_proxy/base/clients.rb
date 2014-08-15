b4968ecf14f0befe663cb243396281634a72778e
y13i <email@y13i.com>
2014-08-15 23:20:18 +0900

    clients api done?

--- lib/sensu_api_proxy/base/clients.rb
+++ lib/sensu_api_proxy/base/clients.rb
@@ -1,16 +1,78 @@
+# Proxy for clients API
+# http://sensuapp.org/docs/latest/api_clients
+
 class SensuAPIProxy::Base
   get "/clients" do
     begin
       call_api do |http|
-        clients = JSON.load http.get("/clients").body
-        intercept_clients(clients).to_json
+        parameters = before_get_clients params
+        request    = "/clients#{params_to_s parameters}"
+        response   = http.get request
+        clients    = load_response response
+
+        after_get_clients(clients).to_json
       end
     rescue
       500
     end
   end
 
-  def intercept_clients clients
+  def before_get_clients parameters
+    parameters
+  end
+
+  def after_get_clients clients
     clients
+  end
+
+  get "/clients/:client" do
+    begin
+      call_api do |http|
+        response = http.get "/clients/#{params[:client]}"
+        client   = load_response response
+
+        after_get_clients_client(client).to_json
+      end
+    rescue
+      500
+    end
+  end
+
+  def after_get_clients_client client
+    client
+  end
+
+  delete "/clients/:client" do
+    begin
+      call_api do |http|
+        response = http.delete "/clients/#{params[:client]}"
+        response = after_delete_clients_client response
+
+        [response.code.to_i, response.body]
+      end
+    rescue
+      500
+    end
+  end
+
+  def after_delete_clients_client response
+    response
+  end
+
+  get "/clients/:client/history" do
+    begin
+      call_api do |http|
+        response = http.get "/clients/#{params[:client]}/history"
+        client   = load_response response
+
+        after_get_clients_client_history(client).to_json
+      end
+    rescue
+      500
+    end
+  end
+
+  def after_get_clients_client_history history
+    history
   end
 end
